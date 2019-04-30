d895dcbbac51da0a1e40e20ac9cafd1a2c2e2fcf
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-04-30 17:11:28 -0600

    app-6, app-7 React2 afternoon project

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Todo from './Todo';
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: ["take garbage", 'wash dishes']
+        
+      
+    };
+
+  }  
+
+  
+
+
   render() {
+    const maplist = this.state.list.map(item => {
+      return(
+        <li>{item}</li>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <ul>{maplist}
+          
+        </ul>
+        
       </div>
     );
   }
 }
+
 
 export default App;
