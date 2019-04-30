4f0cf4ae5dcd147319f530678d5891d42cef2479
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-04-30 15:37:20 -0600

    app-5 react2-afternoon project

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from './Image'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Image url={"http://1.bp.blogspot.com/-ZbMgxHNluXU/ThEGJ3FGuWI/AAAAAAAAAvE/SY37Zk2VMrY/s1600/cat_fight21.jpg"}/>
       </div>
     );
   }
 }
 
 export default App;
+
+
